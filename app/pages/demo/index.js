df620ba1b2d0a91837e5a4e26f7851cd8499b538
goonwzz <wang130@qq.com>
2017-06-13 14:02:28 +0800

    ant

--- app/pages/demo/index.js
+++ app/pages/demo/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "Styles/app.csm";
 import {Link} from "react-router";
+import {Button} from "antd";
 class Demo extends React.Component {
 
     constructor(props) {
@@ -15,7 +16,7 @@ class Demo extends React.Component {
     render() {
 
         return (<div className={styles.demo}>
-            <Link to="/">demo</Link>
+            <Link to="/"><Button>demo</Button></Link>
         </div>)
     }
 
